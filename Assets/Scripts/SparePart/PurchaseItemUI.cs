f988794f9c2b287201451d11d58b1dda95c6b11e
Victor0870 <victory.studio.2024@gmail.com>
2025-09-10 13:57:01 +0700

    Update price vs new material

--- Assets/Scripts/SparePart/PurchaseItemUI.cs
+++ Assets/Scripts/SparePart/PurchaseItemUI.cs
@@ -8,6 +8,8 @@ public class PurchaseItemUI : MonoBehaviour
     public TMP_InputField poNumberInput;
     public TMP_InputField quantityInput;
     public TMP_InputField supplierInput;
+    // --- Thêm trường nhập liệu cho giá ---
+    public TMP_InputField priceInput;
 
     public string materialId { get; private set; }
 

e9d6ce45f660f8c72070b9afc6c7324315b74315
Victor0870 <victory.studio.2024@gmail.com>
2025-09-11 14:44:55 +0700

    Add name task and PIC

--- Assets/Scripts/SparePart/UsageHistoryItemUI.cs
+++ Assets/Scripts/SparePart/UsageHistoryItemUI.cs
@@ -6,8 +6,10 @@ public class UsageHistoryItemUI : MonoBehaviour
 {
     public TextMeshProUGUI quantityText;
     public TextMeshProUGUI timestampText;
+    public TextMeshProUGUI taskNameText;
+    public TextMeshProUGUI createdByText; // Thêm biến mới
 
-    public void SetData(int quantity, DateTime timestamp)
+    public void SetData(int quantity, DateTime timestamp, string taskName, string createdBy) // Thêm tham số createdBy
     {
         if (quantityText != null)
         {
@@ -16,6 +18,15 @@ public class UsageHistoryItemUI : MonoBehaviour
         if (timestampText != null)
         {
             timestampText.text = $"Thời gian: {timestamp.ToString("dd/MM/yyyy HH:mm")}";
+        }
+        if (taskNameText != null)
+        {
+            taskNameText.text = $"Công việc: {taskName}";
+        }
+        // Hiển thị tên người yêu cầu
+        if (createdByText != null)
+        {
+            createdByText.text = $"Người yêu cầu: {createdBy}";
         }
     }
 }
